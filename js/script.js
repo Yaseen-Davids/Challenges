ba50359d303398ae16cb52881b88d366facc61da
Yaseen Davids <yaseendavids477@gmail.com>
2018-05-24 16:55:27 +0200

    All sections added (fix dropdown)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,73 @@
 onload = function headerSmooth() {
     document.getElementById("myHeader").style.width = "34%";
-    setInterval(slogan, 1500);
+    setInterval(slogan, 1200);
     function slogan() {
         document.getElementById("mySlogan").style.width = "17.5%";
+    }    
+}
+
+function box1Info() {
+    let box1 = document.getElementById("box1Info");
+
+    if (box1.style.display === "block"){
+        box1.style.display = "none";
     }
-    
+    else {
+        box1.style.display = "block";
+    }
+}
+
+function box2Info() {
+    let box2 = document.getElementById("box2Info");
+
+    if (box2.style.display === "block"){
+        box2.style.display = "none";
+    }
+    else {
+        box2.style.display = "block";
+    }
+}
+
+function box3Info() {
+    let box3 = document.getElementById("box3Info");
+
+    if (box3.style.display === "block"){
+        box3.style.display = "none";
+    }
+    else {
+        box3.style.display = "block";
+    }
+}
+
+function box4Info() {
+    let box4 = document.getElementById("box4Info");
+
+    if (box4.style.display === "block"){
+        box4.style.display = "none";
+    }
+    else {
+        box4.style.display = "block";
+    }
+}
+
+function box5Info() {
+    let box5 = document.getElementById("box5Info");
+
+    if (box5.style.display === "block"){
+        box5.style.display = "none";
+    }
+    else {
+        box5.style.display = "block";
+    }
+}
+
+function box6Info() {
+    let box6 = document.getElementById("box6Info");
+
+    if (box6.style.display === "block"){
+        box6.style.display = "none";
+    }
+    else {
+        box6.style.display = "block";
+    }
 }
