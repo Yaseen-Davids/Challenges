9a3d7303aa1b10ea734b4469df4025524f5cfa04
Yaseen Davids <yaseendavids477@gmail.com>
2018-05-25 14:31:38 +0200

    Dropdown section updated

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-onload = function headerSmooth() {
-    document.getElementById("myHeader").style.width = "34%";
-    setInterval(slogan, 1200);
-    function slogan() {
-        document.getElementById("mySlogan").style.width = "17.5%";
-    }    
-}
+// onload = function headerSmooth() {
+//     document.getElementById("myHeader").style.width = "34%";
+//     setInterval(slogan, 1200);
+//     function slogan() {
+//         document.getElementById("mySlogan").style.width = "17.5%";
+//     }    
+// }
 
 function box1Info() {
     let box1 = document.getElementById("box1Info");
 
-    if (box1.style.display === "block"){
+    if (box1.style.display === "inline-block"){
         box1.style.display = "none";
     }
     else {
@@ -20,7 +20,7 @@ function box1Info() {
 function box2Info() {
     let box2 = document.getElementById("box2Info");
 
-    if (box2.style.display === "block"){
+    if (box2.style.display === "inline-block"){
         box2.style.display = "none";
     }
     else {
@@ -31,7 +31,7 @@ function box2Info() {
 function box3Info() {
     let box3 = document.getElementById("box3Info");
 
-    if (box3.style.display === "block"){
+    if (box3.style.display === "inline-block"){
         box3.style.display = "none";
     }
     else {
@@ -42,7 +42,7 @@ function box3Info() {
 function box4Info() {
     let box4 = document.getElementById("box4Info");
 
-    if (box4.style.display === "block"){
+    if (box4.style.display === "inline-block"){
         box4.style.display = "none";
     }
     else {
@@ -53,7 +53,7 @@ function box4Info() {
 function box5Info() {
     let box5 = document.getElementById("box5Info");
 
-    if (box5.style.display === "block"){
+    if (box5.style.display === "inline-block"){
         box5.style.display = "none";
     }
     else {
